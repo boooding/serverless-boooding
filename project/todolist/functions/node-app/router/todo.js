82b5b5a16c9bec56177fea1ad8ef168a4ca6a978
boooding <boooding.tang@outlook.com>
2022-07-11 20:00:45 +0800

    vue-todolist initialization

--- project/todolist/functions/node-app/router/todo.js
+++ project/todolist/functions/node-app/router/todo.js
@@ -1,31 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (
+const cloudbase = require("@cloudbase/node-sdk");
+const cloud_config = require('../cloud_config')
+const cloudDb = cloudbase.init(cloud_config);
+const db = cloudDb.database();
+
+router.get('/', async (
     req,
     res) => {
-    res.send('todo router')
+    const todolist = await db.collection('todo').get();
+    res.send(todolist)
 })
 
 // add todo
-router.post('/', (
+router.post('/', async (
     req,
     res) => {
-    res.send('todo post')
+    if (!req.body.title) {
+        return;
+    }
+    const todoContent = {
+        title: req.body.title,
+        createTime: Date.now(),
+        done:false
+    }
+    const back_data = await db.collection('todo').add(todoContent)
+    res.send(back_data)
 })
 
 // edit todo
-router.put('/', (
+router.put('/', async (
     req,
     res) => {
-    res.send('todo put')
+    const todoContent = {
+        title: req.query.title,
+        done:req.query.done
+    }
+    const back_data = await db.collection('todo').doc(req.query.id).update(todoContent)
+    res.send(back_data)
 })
 
 // delete todo
-router.delete('/', (
+router.delete('/',  async (
     req,
     res) => {
-    res.send('todo delete')
+    const back_data = await db.collection('todo').doc(req.query.id).remove()
+    res.send(back_data)
 })
 
 
